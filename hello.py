7a7594e0313c26fb0c4510dcd4338286e3cb6f73
Ethanbahr <bahr.e@stu.janesville.k12.wi.us>
2025-01-28 15:07:20 -0600

    Update 1/28

--- hello.py
+++ hello.py
@@ -1,1 +1,25 @@
-print("Hello, World!")
+print("Hello, World!")\
+"""
+int = integar
+float = number w/ decimal
+str = string - any text in quotes
+chr = character - single keyboard character in single quotes
+bool = boolean - true or false
+
++ = add
+- = subtract
+* = multiply
+/ = divide
+// = floor divide - divide and round down to nearest int
+% = mod - remainder from int division
+** - pow - exponent
+
+= - value assignment
++= - x = x + y
+-= - x = x - y
+*=
+/=
+//=
+%=
+**=
+"""
