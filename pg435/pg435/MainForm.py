9464929d80b16c92ac999d2df2b2a87dba1d6efc
Ethanbahr <bahr.e@stu.janesville.k12.wi.us>
2025-02-17 15:26:26 -0600

    Update 2/17

--- pg435/pg435/MainForm.py
+++ pg435/pg435/MainForm.py
@@ -18,58 +18,66 @@ class MainForm(Form):
 		# 
 		# label1
 		# 
-		self._label1.BackColor = System.Drawing.Color.WhiteSmoke
-		self._label1.Location = System.Drawing.Point(29, 48)
+		self._label1.BackColor = System.Drawing.Color.Snow
+		self._label1.Font = System.Drawing.Font("Rockwell", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label1.Location = System.Drawing.Point(29, 38)
 		self._label1.Name = "label1"
-		self._label1.Size = System.Drawing.Size(100, 23)
+		self._label1.Size = System.Drawing.Size(120, 46)
 		self._label1.TabIndex = 0
 		self._label1.Text = "General Sales"
 		self._label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
 		# 
 		# label2
 		# 
-		self._label2.BackColor = System.Drawing.Color.WhiteSmoke
-		self._label2.Location = System.Drawing.Point(29, 118)
+		self._label2.BackColor = System.Drawing.Color.Snow
+		self._label2.Font = System.Drawing.Font("Rockwell", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label2.Location = System.Drawing.Point(29, 99)
 		self._label2.Name = "label2"
-		self._label2.Size = System.Drawing.Size(100, 23)
+		self._label2.Size = System.Drawing.Size(120, 46)
 		self._label2.TabIndex = 1
 		self._label2.Text = "Student Sales"
 		self._label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
 		# 
 		# button1
 		# 
-		self._button1.Location = System.Drawing.Point(272, 29)
+		self._button1.BackColor = System.Drawing.Color.Snow
+		self._button1.Font = System.Drawing.Font("Rockwell", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.Location = System.Drawing.Point(186, 29)
 		self._button1.Name = "button1"
-		self._button1.Size = System.Drawing.Size(99, 50)
+		self._button1.Size = System.Drawing.Size(111, 64)
 		self._button1.TabIndex = 2
 		self._button1.Text = "General Sales"
-		self._button1.UseVisualStyleBackColor = True
+		self._button1.UseVisualStyleBackColor = False
 		self._button1.Click += self.Button1Click
 		# 
 		# button2
 		# 
-		self._button2.Location = System.Drawing.Point(272, 99)
+		self._button2.BackColor = System.Drawing.Color.Snow
+		self._button2.Font = System.Drawing.Font("Rockwell", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button2.Location = System.Drawing.Point(186, 99)
 		self._button2.Name = "button2"
-		self._button2.Size = System.Drawing.Size(99, 50)
+		self._button2.Size = System.Drawing.Size(111, 64)
 		self._button2.TabIndex = 3
 		self._button2.Text = "Student Sales"
-		self._button2.UseVisualStyleBackColor = True
+		self._button2.UseVisualStyleBackColor = False
 		self._button2.Click += self.Button2Click
 		# 
 		# button3
 		# 
-		self._button3.Location = System.Drawing.Point(272, 170)
+		self._button3.BackColor = System.Drawing.Color.Snow
+		self._button3.Font = System.Drawing.Font("Rockwell", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button3.Location = System.Drawing.Point(186, 169)
 		self._button3.Name = "button3"
-		self._button3.Size = System.Drawing.Size(99, 50)
+		self._button3.Size = System.Drawing.Size(111, 55)
 		self._button3.TabIndex = 4
 		self._button3.Text = "Exit"
-		self._button3.UseVisualStyleBackColor = True
+		self._button3.UseVisualStyleBackColor = False
 		self._button3.Click += self.Button3Click
 		# 
 		# MainForm
 		# 
-		self.BackColor = System.Drawing.Color.Gray
-		self.ClientSize = System.Drawing.Size(426, 313)
+		self.BackColor = System.Drawing.Color.Tomato
+		self.ClientSize = System.Drawing.Size(319, 246)
 		self.Controls.Add(self._button3)
 		self.Controls.Add(self._button2)
 		self.Controls.Add(self._button1)
