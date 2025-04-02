b684f1fc2f7e91efac420c9336ad2c2fbb34b05a
Ethanbahr <bahr.e@stu.janesville.k12.wi.us>
2025-04-02 15:28:09 -0500

    Update 4/2

---
+++ Pg479conference/Pg479conference/MainForm.py
@@ -0,0 +1,78 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._button1 = System.Windows.Forms.Button()
+		self._button2 = System.Windows.Forms.Button()
+		self._button3 = System.Windows.Forms.Button()
+		self.SuspendLayout()
+		# 
+		# button1
+		# 
+		self._button1.BackColor = System.Drawing.Color.HotPink
+		self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 9.75, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.ForeColor = System.Drawing.Color.Teal
+		self._button1.Location = System.Drawing.Point(12, 227)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(115, 60)
+		self._button1.TabIndex = 0
+		self._button1.Text = "Select Conference Options"
+		self._button1.UseVisualStyleBackColor = False
+		self._button1.Click += self.Button1Click
+		# 
+		# button2
+		# 
+		self._button2.BackColor = System.Drawing.Color.HotPink
+		self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 9.75, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+		self._button2.ForeColor = System.Drawing.Color.Teal
+		self._button2.Location = System.Drawing.Point(133, 227)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(115, 60)
+		self._button2.TabIndex = 1
+		self._button2.Text = "Reset"
+		self._button2.UseVisualStyleBackColor = False
+		self._button2.Click += self.Button2Click
+		# 
+		# button3
+		# 
+		self._button3.BackColor = System.Drawing.Color.HotPink
+		self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 9.75, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+		self._button3.ForeColor = System.Drawing.Color.Teal
+		self._button3.Location = System.Drawing.Point(254, 227)
+		self._button3.Name = "button3"
+		self._button3.Size = System.Drawing.Size(115, 60)
+		self._button3.TabIndex = 2
+		self._button3.Text = "Exit"
+		self._button3.UseVisualStyleBackColor = False
+		self._button3.Click += self.Button3Click
+		# 
+		# MainForm
+		# 
+		self.BackColor = System.Drawing.Color.Olive
+		self.ClientSize = System.Drawing.Size(378, 299)
+		self.Controls.Add(self._button3)
+		self.Controls.Add(self._button2)
+		self.Controls.Add(self._button1)
+		self.Name = "MainForm"
+		self.Text = "Pg479conference"
+		self.ResumeLayout(False)
+
+
+	def Button1Click(self, sender, e):
+		from Form1 import *		
+		Form1 = Form1
+		self.Form1.Show()
+		self.Hide()
+
+	def Button2Click(self, sender, e):
+		pass
+
+	def Button3Click(self, sender, e):
+		Application.Exit()
---
+++ Pg479conference/Pg479conference2.py
@@ -0,0 +1,77 @@
+﻿
+import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class Form1(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._button3 = System.Windows.Forms.Button()
+		self._button2 = System.Windows.Forms.Button()
+		self._button1 = System.Windows.Forms.Button()
+		self.SuspendLayout()
+		# 
+		# button3
+		# 
+		self._button3.BackColor = System.Drawing.Color.HotPink
+		self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 9.75, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+		self._button3.ForeColor = System.Drawing.Color.Teal
+		self._button3.Location = System.Drawing.Point(253, 189)
+		self._button3.Name = "button3"
+		self._button3.Size = System.Drawing.Size(115, 60)
+		self._button3.TabIndex = 5
+		self._button3.Text = "Exit"
+		self._button3.UseVisualStyleBackColor = False
+		self._button3.Click += self.Button3Click
+		# 
+		# button2
+		# 
+		self._button2.BackColor = System.Drawing.Color.HotPink
+		self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 9.75, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+		self._button2.ForeColor = System.Drawing.Color.Teal
+		self._button2.Location = System.Drawing.Point(132, 189)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(115, 60)
+		self._button2.TabIndex = 4
+		self._button2.Text = "Clear"
+		self._button2.UseVisualStyleBackColor = False
+		self._button2.Click += self.Button2Click
+		# 
+		# button1
+		# 
+		self._button1.BackColor = System.Drawing.Color.HotPink
+		self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 9.75, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.ForeColor = System.Drawing.Color.Teal
+		self._button1.Location = System.Drawing.Point(11, 189)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(115, 60)
+		self._button1.TabIndex = 3
+		self._button1.Text = "Temporary Text"
+		self._button1.UseVisualStyleBackColor = False
+		self._button1.Click += self.Button1Click
+		# 
+		# Form1
+		# 
+		self.BackColor = System.Drawing.Color.Olive
+		self.ClientSize = System.Drawing.Size(380, 261)
+		self.Controls.Add(self._button3)
+		self.Controls.Add(self._button2)
+		self.Controls.Add(self._button1)
+		self.Name = "Form1"
+		self.Text = "Form1"
+		self.ResumeLayout(False)
+
+
+	def Button1Click(self, sender, e):
+		pass
+
+	def Button2Click(self, sender, e):
+		pass
+
+	def Button3Click(self, sender, e):
+		MainForm.Show()
+		Self.Hide
