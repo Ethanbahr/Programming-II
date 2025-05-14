8bf4bdf270c61ee3dc051134dc98564ad1a7edea
Ethanbahr <bahr.e@stu.janesville.k12.wi.us>
2025-05-14 15:26:10 -0500

    Update 5/14

--- CSharp/Prog122d/Form1.cs
+++ CSharp/Prog122d/Form1.cs
@@ -10,12 +10,12 @@ namespace Prog122d
         private void button1_Click(object sender, EventArgs e)
         {
             listBox1.Items.Clear();
-            foreach (int num in Range(-12, 17)) 
+            listBox1.Items.Add("X:\t\tY:");
+            for (int x = -12; x <= 16; x++)
             {
-            int ans = ((num**6) + (num**5 * (0 - 3)) + (num**4 * (0 - 93)) + (num**3 * 87) + (num**2 * 1596) - (num * (0 - 1380)) - 2800);
-            listBox1.Items.Add(ans);
+                double ans = ((Math.Pow(x, 6)) + ((Math.Pow(x, 5)) * (0 - 3)) + ((Math.Pow(x, 4)) * (0 - 93)) + ((Math.Pow(x, 3)) * 87) + ((Math.Pow(x, 2)) * 1596) - (x * (0 - 1380)) - 2800);
+                listBox1.Items.Add(x + "\t\t" + ans);
             }
-            // ans = ((num**6) + (num **5 * (0-3)) + (num **4 *(0 - 93)) + (num **3 * 87) + (num **2 * 1596) - (num * (0 - 1380)) - 2800)
         }
 
         private void button2_Click(object sender, EventArgs e)
