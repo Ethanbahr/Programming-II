02052c6ab77f141823e682d2ee8dcb27879433df
Ethanbahr <bahr.e@stu.janesville.k12.wi.us>
2025-05-16 15:27:11 -0500

    Update 5/16

---
+++ TKinter test 2/TKinter test 2/Program.py
@@ -0,0 +1,12 @@
+﻿import tkinter as tk
+
+# Create the main window
+window = tk.Tk()
+window.title("My Tkinter App")
+
+# Create a button widget
+button = tk.Button(window, text="Click Me")
+button.pack()
+
+# Start the main event loop
+window.mainloop()
---
+++ TKinter test thingy/TKinter test thingy/MainForm.py
@@ -0,0 +1,29 @@
+﻿import System.Drawing
+import System.Windows.Forms
+import tkinter as tk
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		# 
+		# MainForm
+		# 
+		self.Name = "MainForm"
+		self.Text = "TKinter test thingy"
+
+# Create the main window
+window = tk.Tk()
+window.title("My Tkinter App")
+
+# Create a button widget
+button = tk.Button(window, text="Click Me")
+button.pack()
+
+# Start the main event loop
+window.mainloop()
+
